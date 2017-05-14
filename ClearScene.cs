ba16ab06becfc2912a4973e286f3a598aa8541c7
katuyayoshino <katuya_mail@jade.plala.or.jp>
2017-05-14 12:41:34 +0900

    Add files via upload

---
+++ ClearScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 吉野勝也担当
+/// Clear scene.
+/// </summary>
+
+public class ClearScene : MonoBehaviour 
+{
+
+	public void ButtonPush()
+	{
+		Application.LoadLevel ("Title");
+	}
+}
